4d41a7a5bb7fced3dcab8f59a19eadc7c85a63ff
xlgwr <xlgwr@163.com>
2013-10-18 10:39:18 +0800

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Static Pages" do 
+  describe "Home pges" do
+    it "hould have the conternt 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+      expect(page).to have_title("Ruby on Rails by xlgwr | Home")
+    end
+  end
+  
+  describe "Home pges" do
+    it "hould have the conternt 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      expect(page).to have_title("Ruby on Rails by xlgwr | Help")
+    end
+  end
+  
+  describe "About page" do
+    it "hould have the conternt 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+      expect(page).to have_title("Ruby on Rails by xlgwr | About Us")
+    end
+  end
+  
+  
+end
