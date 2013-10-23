47807298992f3e4ff3e0e86c8a3faad6467cd7e2
xlgwr <xlgwr@163.com>
2013-10-23 18:29:57 +0800

    add follow and unfollow

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,6 +2,6 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
-  validates :followed_id
+  validates :followed_id, presence: true
 
 end
