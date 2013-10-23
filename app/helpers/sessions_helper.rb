fca0dd32d522e670a5a17935a956bc52a452d83f
xlgwr <xlgwr@163.com>
2013-10-23 17:36:10 +0800

    update rm #

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,9 @@ module SessionsHelper
   def current_user
     remember_token=User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+    rescue
+    sign_out
+    redirect_to root_url
   end
   def sign_out
     self.current_user=nil
